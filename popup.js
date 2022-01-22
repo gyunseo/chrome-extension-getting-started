f82adb27bc429d16d23da69a7fffccf4e92360b7
Gyunseo Lee <rbstj0311@naver.com>
2022-01-22 23:26:04 +0900

    turns the background color of the page the same color as the button add the activeTab and scripting permissions

--- popup.js
+++ popup.js
@@ -5,3 +5,19 @@ chrome.storage.sync.get("color", ({ color }) => {
     changeColor.style.backgroundColor = color;
 });
 //storage에서 color값을 가져오고, 버튼을 그 값으로 바꾼다.
+
+changeColor.addEventListener("click", async () => {
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true});
+
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: setPageBackroundColor,
+    });
+
+});
+
+function setPageBackroundColor() {
+    chrome.storage.sync.get("color", ({ color }) => {
+        document.body.style.backgroundColor = color;
+    });
+}
